d82cea15f597999ab8cb33eff7827cc723e74e92
Divhani-TN <divhanithibajr@gmail.com>
2023-10-19 14:19:41 +0200

    Background image added

---
+++ Assets/Scripts/Interactable Objects.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObjects : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
