87e812697fe85eb71253c19127ca52be3df89cb9
Divhani-TN <divhanithibajr@gmail.com>
2023-10-27 17:12:09 +0200

    Work done
    Button issue fixed

--- Assets/Scripts/Scenemanagement.cs
+++ Assets/Scripts/Scenemanagement.cs
@@ -6,7 +6,7 @@ public class Scenemanagement : MonoBehaviour
 {
 
     // Start is called before the first frame update
-    public GameObject PauseMenu;
+    
     //public AudioSource audioSource;
     public void LoadMenu()
     {
@@ -15,7 +15,7 @@ public class Scenemanagement : MonoBehaviour
 
     public void LoadGame()
     {
-        SceneManager.LoadScene("Actual Game", LoadSceneMode.Single);
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
         //audioSource.Play();
     }
     public void loadSettings()
@@ -23,46 +23,8 @@ public class Scenemanagement : MonoBehaviour
         SceneManager.LoadScene("Settings", LoadSceneMode.Single);
     }
 
-    public void Start()
-    {
-        PauseMenu.SetActive(false);
-    }
-    public void Update()
-    {
-
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            PauseMenu.SetActive(true);
-            Time.timeScale = 0f;
-            //Cursor.lockState = CursorLockMode.None;
-            //Cursor.visible = true;
-        }
-        if (PauseMenu.activeSelf)
-        {
-            if (Input.GetKeyDown(KeyCode.M))
-            {
-                PauseMenu.SetActive(false);
-                SceneManager.LoadScene("Menu");
-            }
-
-            if (Input.GetKeyDown(KeyCode.R))
-            {
-                PauseMenu.SetActive(false);
-                Time.timeScale = 1.0f;
-                SceneManager.LoadScene("Actual Game");
-            }
-            if (Input.GetKeyDown(KeyCode.U))
-            {
-                PauseMenu.SetActive(false);
-                Time.timeScale = 1.0f;
-            }
-        }
-
-
-    }
-
     public void LoadHowToPlay()
     {
-        SceneManager.LoadScene("HowToPlay", LoadSceneMode.Single);
+        SceneManager.LoadScene("Tutorial", LoadSceneMode.Single);
     }
 }
