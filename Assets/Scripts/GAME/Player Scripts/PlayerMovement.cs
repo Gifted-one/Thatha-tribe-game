4bc4554f4dafc412bbee99f7336d321797e8eee9
kingkhado <144692407+kingkhado@users.noreply.github.com>
2023-11-03 18:21:19 +0200

    finalised player movement
    Movement + animation

--- Assets/Scripts/GAME/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/GAME/Player Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -16,16 +17,26 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _movementInput;
     private Vector2 _smoothedMovementInput;
     private Vector2 _movementInputSmoothVelocity;
+    private Animator _animator;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
         SetPlayerVelocity();
         RotateInDirectionOfInput();
+        SetAnimation();
+    }
+
+    private void SetAnimation()
+    {
+        bool isMoving = _movementInput != Vector2.zero;
+
+        _animator.SetBool("IsMoving", isMoving);
     }
 
     private void SetPlayerVelocity()
@@ -55,7 +66,7 @@ public class PlayerMovement : MonoBehaviour
         _movementInput = inputValue.Get<Vector2>();
     }
 
-   
+
 
     Vector2 movement;
 
@@ -65,7 +76,7 @@ public class PlayerMovement : MonoBehaviour
         RotateHand();
     }
 
-    
+
 
     void RotateHand()
     {
@@ -81,4 +92,5 @@ public class PlayerMovement : MonoBehaviour
         movement = new Vector2(mx, my).normalized;
     }
 }
+
 
